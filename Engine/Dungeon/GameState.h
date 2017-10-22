76cb0d95782a82aebb08676b5c923520f25dafe8
Eric Kenneth Marquez <kenkm96@hotmail.com>
2017-10-21 18:39:33 -0700

    -update to comments

--- Engine/Dungeon/GameState.h
+++ Engine/Dungeon/GameState.h
@@ -28,16 +28,19 @@ public:
 	//(= default) just means there wont be any special constructor for this class and the compiler will instead do nothing for this function
 	//-Alternatively you can just delete it but I do it for organization or if I ever want to write one
 	GameState() = default;
+
 	//Function called every frame
-	//Looks for non-zero on exit
+	//Looks for GameStates::States::(state to change to) on return
 	//All gamestates will require the (elapsed time for the frame) and (controller inmput) function.
 	virtual GameStates::States Update(float time, std::shared_ptr<Lime::InputManager>& input) = 0;
 
 	//State Changes
 	//All of the code that has heavy calculations should be done in this function before the GS loads
 	virtual void Initialize(int windowWidth, int windowHeight) = 0;
+
 	//All the code that requires you to unalocate, clear or release is done here
 	virtual void Close() = 0;
+
 	//This function is used to update objects that require the window specs
 	//!Notice! this functon does not have an ( = 0 ) at the end
 	//-This means that the function does not need to be in the class that inherits this class
