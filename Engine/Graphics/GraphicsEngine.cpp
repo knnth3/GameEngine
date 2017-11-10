2c584e0a761944c2994e8bd5519b5124f3c1b461
Eric Kenneth Marquez <kenkm963@gmail.com>
2017-11-10 09:48:10 -0800

    -Moved TestGame, System, and Graphics beta out of main sln
    -Now only located in the beta folder

--- Engine/Graphics/GraphicsEngine.cpp
+++
@@ -1,8 +0,0 @@
-#include "GraphicsEngine.h"
-
-
-
-bool Graphics::GraphicsEngine::Initialize(const void * window, const uint32_t width, const uint32_t height)
-{
-	return false;
-}
--- Engine/TestGame/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-
-
-int main()
-{
-	std::unique_ptr<System::App> app = std::make_unique<Game>();
-	int result = System::RunApp(L"Test Game", app);
-	return result;
-}
